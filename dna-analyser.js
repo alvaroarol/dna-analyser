0ec9cb2738603433dd0b1b3cf286ee244e02670a
Alvaro Arroyo Olalla <alvaro.arol@gmail.com>
2016-06-08 20:49:51 +0200

    Update dna-analyser.js

--- dna-analyser.js
+++ dna-analyser.js
@@ -62,93 +62,82 @@ var translation = function(sequence){
 
 //Cuts out the translated sequence between codon start (first Met) to codon STOP
 var translationShort = function (protseq){
-	
-	//Checks if any Met protein in the array
+	//If the translated sequence doesn't contain a start codon (Met), return nothing
 	if(protseq.indexOf("Met") === -1){
 		return [];
-	
-	}else{
+	}
+	else{
 		var a = [];
 		var b = protseq;
 		var loop = 1;
 		var proteins = [];
 		var start = 0;
-	var end = 0;
+		var end = 0;
 	
-	do{
-		start = b.indexOf("Met");
-		
-		//remove all protein before first Met
-		a = b.slice(start, b.length + 1);
-		
-		//get end of current protein interval
-		if(a.indexOf("STOP") === -1){
-			end = a.length + 1;
-			loop = 0;
-		}else{
-			end = a.indexOf("STOP");
-		}
-	
-		//get interval between first met and first STOP
-		b = a.slice(0, end);
-		proteins.push(b.join(" "));
-	
-		//remove all protein before first STOP including the STOP itself
-		b = a.slice(end + 1, a.length + 1);
-		a = b;
-		
-		if(b.indexOf("Met") === -1){
-			loop = 0;
-		}
-	}while(loop);
-	
-	return proteins;
+		do{
+			start = b.indexOf("Met");
+			//Remove the translated sequence before first Met
+			a = b.slice(start, b.length + 1);
+			//If there isn't a STOP codon after the previously found Met, the protein ends at the end of the sequence
+			if(a.indexOf("STOP") === -1){
+				end = a.length + 1;
+				loop = 0;
+			}
+			//Else, find the STOP codon of the current protein 
+			else{
+				end = a.indexOf("STOP");
+			}
+			//Add the protein found from Met to STOP into the protein array
+			b = a.slice(0, end);
+			proteins.push(b.join(" "));
+			//Remove everything before the last used STOP codong(including the STOP itself), for next loop iteration
+			b = a.slice(end + 1, a.length + 1);
+			a = b;
+			//If there isn't any Met left after that, end the loop
+			if(b.indexOf("Met") === -1){
+				loop = 0;
+			}
+		} while(loop);
+		return proteins;
 	}
 };
 
 //Writes the result of the function to HTML
 var writeToHtml = function(result,title,id,button){
-	
-	//create a div element and give it an id
+	//Create a div element and give it an id
 	var newdiv = document.createElement("div");
 	newdiv.id = id;
-	
-	//create a p element and set its content
+	//Create a p element and set its content
 	var newp = document.createElement("p");
 	newp.innerHTML = title;
-	
-	//Create a span element, give it and id and create the result text
+	//Create a span element, give it an id and create the result text
 	var newspan = document.createElement("span");
 	newspan.id = id + "span";
 	newspan.innerHTML = result;
-	
 	//Append the p and text to the div
 	newdiv.appendChild(newp);
 	newdiv.appendChild(newspan);
-	
 	//Append the div to the div #results
 	document.getElementById("results").appendChild(newdiv);
-	
 	//If specified and if the result isn't blank, create a show/hide result button (to tidy up the page)
 	if(button && (result != "")){
 		//Create button, class and text
 		var newbutton = document.createElement("button");
 		newbutton.className = "show";
 		newbutton.innerHTML = "Show";
-	
 		//Onclick calls a function that switches the button and span between show and hide
 		newbutton.addEventListener("click", function(){
 			if(newbutton.className === "show"){
 				newspan.style.cssText = "display: block;";
 				newbutton.innerHTML = "Hide";
 				newbutton.className = "hide";
-			}else{
+			}
+			else{
 				newspan.style.cssText = "display: none;";
 				newbutton.innerHTML = "Show";
 				newbutton.className = "show";
 			}
 		});
-	
 		//Append button to p
 		newp.appendChild(newbutton);
 	}
@@ -156,36 +145,33 @@ var writeToHtml = function(result,title,id,button){
 
 //Main function
 var analyseDNA = function(){
-	
-	//erases all previous analysis results
+	//Erases all previous analysis results
 	document.getElementById("results").innerHTML = "";
-	
-	//stores submitted DNA sequence
+	//Gets submitted DNA sequence
 	var submittedSequence = document.getElementById("DNA").value;
-	
 	//Formats the sequence to have a continuous series of letters (removes numbers, blanks, hyphens, tabs and line breaks)
 	var formattedSequence = submittedSequence.split(/[0-9]|\s|\n|\t|\/|\-/g).join("").toUpperCase();
-	
+	//If sequence hasn't got any non-nucleotide characters, analyse it
 	if(isSequenceValid(formattedSequence)){
-		//Formatted Sequence
+		//Format Sequence
 		writeToHtml(formattedSequence,"Formatted sequence", "formattedseq",1);
-	
 		//Nucleotide Frequency
 		writeToHtml(nucleotideFrequency(formattedSequence).join(" "), "Nucleotide frequencies in percentage (A, T, G, C) ", "nucleotidefreq",1);
-
+		//Translations for 3 possible starting positions
 		var translatedSequences = [
 			translation(formattedSequence), 
 			translation(formattedSequence.slice(1, formattedSequence.length + 1)), 
 			translation(formattedSequence.slice(2, formattedSequence.length + 1))
 		];
-		
-		for(var i = 0; i < translatedSequences.length; i++){
-			//Translation from first position
+		for(var i = 1; i <= translatedSequences.length; i++){
+			//Translation
 			writeToHtml(translatedSequences[i].join(" "), "Translation prediction from first nucleotide position ", "translatedseq" + i, 1);
-			//Possible protein from first position
+			//Possible protein
 			writeToHtml(translationShort(translatedSequences[i]).join("<br/><br/>"), "Possible proteins ", "shorttranslatedseq" + i, 1);
 		}
-	}else{
-		alert("The sequence contains at least one forbidden, non-nucleotide character!");
+	}
+	//If there are non-nucleotide characters, abort and warn user
+	else{
+		alert("The sequence contains at least one forbidden character! (See description above input box)");
 	}
 };
